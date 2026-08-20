2f34eb9b56cb56499eab027496c4b1b9203de281
orbitalbgmi-cpu <orbitalbgmi@gmail.com>
2026-08-20 13:13:58 +0530

    Update ChatMessage.cs

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -4,6 +4,9 @@ public class ChatMessage
 {
     public string Text { get; set; } = string.Empty;
     public bool IsUser { get; set; }
+    public ImageSource? Image { get; set; }
+    public bool HasImage => Image != null;
+    public bool HasText => !string.IsNullOrEmpty(Text);
     public Color BubbleColor => IsUser ? Color.FromArgb("#1f6feb") : Color.FromArgb("#21262d");
     public LayoutOptions Alignment => IsUser ? LayoutOptions.End : LayoutOptions.Start;
 }
